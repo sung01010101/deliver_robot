96cad43b2714b8365649ca933a8274264e923110
sung <howardsung0714@gmail.com>
2025-07-15 10:52:35 +0800

    message

--- launch/cartographer_lidar_only.launch.py
+++ launch/cartographer_lidar_only.launch.py
@@ -1,10 +1,26 @@
+"""
+  Copyright 2018 The Cartographer Authors
+  Copyright 2022 Wyca Robotics (for the ros2 conversion)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, SetRemap
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 """
@@ -28,28 +44,20 @@ def generate_launch_description():
 
     ## ***** File paths ******
     pkg_share = FindPackageShare('deliver_robot').find('deliver_robot')
-    urdf_dir = os.path.join(pkg_share, 'description')
-    urdf_file = os.path.join(urdf_dir, 'robot_model.urdf')
-    with open(urdf_file, 'r') as infp:
-        robot_desc = infp.read()
+    # urdf_dir = os.path.join(pkg_share, 'description')
+    # urdf_file = os.path.join(urdf_dir, 'robot_model.urdf')
+    # with open(urdf_file, 'r') as infp:
+    #     robot_desc = infp.read()
 
-    ## ***** Nodes *****
-    robot_state_publisher_node = Node(
-        package = 'robot_state_publisher',
-        executable = 'robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_desc},
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}],
-        output = 'screen'
-        )
-
-    ## ***** base_link -> laser *****
-    static_transform_publisher = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'laser'],
-        output='screen'
-    )
+    # ## ***** Nodes *****
+    # robot_state_publisher_node = Node(
+    #     package = 'robot_state_publisher',
+    #     executable = 'robot_state_publisher',
+    #     parameters=[
+    #         {'robot_description': robot_desc},
+    #         {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+    #     output = 'screen'
+    #     )
 
     cartographer_node = Node(
         package = 'cartographer_ros',
@@ -66,8 +74,9 @@ def generate_launch_description():
         package = 'cartographer_ros',
         executable = 'cartographer_occupancy_grid_node',
         parameters = [
-            {'use_sim_time': False},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
             {'resolution': 0.05}],
+        output = 'screen'
         )
     
     rviz_dir = os.path.join(pkg_share, 'rviz')
@@ -80,26 +89,9 @@ def generate_launch_description():
         name='rviz2',
         arguments=['-d', rviz_path],
         output='screen')
-    
-    # 顯示轉動 gui    
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
-    
-    # 顯示可以轉的元件（車輪）
-    joint_state_publisher_gui_node = Node(
-    	package='joint_state_publisher_gui',
-    	executable='joint_state_publisher_gui',
-    	name='joint_state_publisher_gui',
-    	condition=IfCondition(LaunchConfiguration('gui')),
-    	parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
-	)
         
     return LaunchDescription([
         use_sim_time_arg,
-        gui_arg,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        static_transform_publisher,
         rviz_node,
         cartographer_node,
         cartographer_occupancy_grid_node,
--- launch/cartographer_mapping.launch.py
+++ launch/cartographer_mapping.launch.py
@@ -1,10 +1,26 @@
+"""
+  Copyright 2018 The Cartographer Authors
+  Copyright 2022 Wyca Robotics (for the ros2 conversion)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, SetRemap
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 """
@@ -47,7 +63,7 @@ def generate_launch_description():
     static_transform_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'laser'],
+        arguments=['0', '0', '0.3', '0', '0', '0', 'base_link', 'laser'],
         output='screen'
     )
 
