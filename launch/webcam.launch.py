6988691e3d8ce9337486a7cb993cd0b94737df98
sung <howardsung0714@gmail.com>
2025-07-30 13:45:18 +0800

    add webcam launch file

--- launch/webcam.launch.py
+++ launch/webcam.launch.py
@@ -2,6 +2,8 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -9,19 +11,36 @@ def generate_launch_description():
     width = DeclareLaunchArgument('width', default_value='640')
     height = DeclareLaunchArgument('height', default_value='480')
     freq = DeclareLaunchArgument('freq', default_value='30.0')
-    device_id = DeclareLaunchArgument('device_id', default_value='0')  # Camera device ID (0 for default camera)
+    device_id = DeclareLaunchArgument('device_id', default_value='0')  # Camera device ID (/dev/video0)
+    view_vid = DeclareLaunchArgument('view_vid', default_value='False')
+
+    webcam_node = Node(
+        package='image_tools',
+        executable='cam2image',
+        name='webcam_node',
+        output='screen',
+        parameters=[
+            {'width': LaunchConfiguration('width')},
+            {'height': LaunchConfiguration('height')},
+            {'freq': LaunchConfiguration('freq')},
+            {'device_id': LaunchConfiguration('device_id')},
+        ]
+    )
+
+    view_vid_node = Node(
+        package='rqt_image_view',
+        executable='rqt_image_view',
+        name='image_viewer',
+        output='screen',
+        condition=IfCondition(LaunchConfiguration('view_vid'))
+    )
 
     return LaunchDescription([
-        Node(
-            package='image_tools',
-            executable='cam2image',
-            name='webcam_node',
-            output='screen',
-            parameters=[
-                {'width': width},
-                {'height': height},
-                {'freq': freq},
-                {'device_id': device_id},
-            ]
-        )
+        width,
+        height,
+        freq,
+        device_id,
+        view_vid,
+        webcam_node,
+        view_vid_node
     ])
